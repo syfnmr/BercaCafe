c1825bd6a7cdc95de7f95c3d51cac6129c39530f
syfnmr <syfnmr12@gmail.com>
2022-09-23 13:58:34 +0700

    NweReportCup

--- BercaCafe_API/ViewModels/ReportByCupVM.cs
+++ BercaCafe_API/ViewModels/ReportByCupVM.cs
@@ -8,8 +8,8 @@ namespace BercaCafe_API.ViewModels
         public string Name { get; set; }
 
         public string DivisionName { get; set; }
-        public int JmlIce { get; set; } 
-        public int JmlHot { get; set; }
+        public int MyCup { get; set; } 
+        public int YourCup { get; set; }
         public DateTime fromDate { get; set; }
         public DateTime thruDate { get; set; }
 
